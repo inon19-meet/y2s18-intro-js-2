c1236886783841507a5615f8c5a257e569ab6db7
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:14:31 +0300

    g

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
-// Create your 'me' object literal here!
+var me ={
+	firstname:"inon", 
+	lastname:"amar", 
+	age:"17", 
+	favoritefoods:"shawarma" "pizza", 
+	picture: "https://www.google.co.il/imgres?imgurl=http%3A%2F%2Fquotesnhumor.com%2Fwp-content%2Fuploads%2F2014%2F10%2FBest-Funny-pics-of-the-week-humor.jpg&imgrefurl=http%3A%2F%2Fquotesnhumor.com%2Ftop-funny-pictures-and-jokes-of-the-week%2F&docid=mGm4qSTk3CvZDM&tbnid=6fIWS1QuZC3OuM%3A&vet=10ahUKEwjFxNzJvcvcAhUFDMAKHeUOC6YQMwhIKBEwEQ..i&w=600&h=599&hl=en&bih=639&biw=1299&q=funny%20pictures&ved=0ahUKEwjFxNzJvcvcAhUFDMAKHeUOC6YQMwhIKBEwEQ&iact=mrc&uact=8"
+	
+}
+
